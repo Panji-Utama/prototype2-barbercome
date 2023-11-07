7ac4d51dc14da0b07271b412430266c4f4ab22ed
FIRMAN PANJI <fpu2468@gmail.com>
2023-11-08 00:38:07 +0700

    fix: Updated Login and Register page for Customer

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,46 @@
-// import 'package:flutter/material.dart';
-// import 'login.dart';
-// import 'package:firebase_messaging/firebase_messaging.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
-
-// MaterialColor customColor = MaterialColor(0xFFD06674, {
-//   50: Color(0xFFD06674),
-//   100: Color(0xFFD06674),
-//   200: Color(0xFFD06674),
-//   300: Color(0xFFD06674),
-//   400: Color(0xFFD06674),
-//   500: Color(0xFFD06674), // 500 is the primary color value
-//   600: Color(0xFFD06674),
-//   700: Color(0xFFD06674),
-//   800: Color(0xFFD06674),
-//   900: Color(0xFFD06674),
-// });
-
-// Future<void> main() async {
-//   runApp(const MyApp());
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//   WidgetsFlutterBinding.ensureInitialized();
-//   String? token = await FirebaseMessaging.instance.getToken();
-//   print("This is the token : $token");
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       debugShowCheckedModeBanner: false,
-//       theme: ThemeData(
-//         primarySwatch: MaterialColor(0xFFFF6E40, {
-//           50: Color(0xFFFF6E40),
-//           100: Color(0xFFFF6E40),
-//           200: Color(0xFFFF6E40),
-//           300: Color(0xFFFF6E40),
-//           400: Color(0xFFFF6E40),
-//           500: Color(0xFFFF6E40),
-//           600: Color(0xFFFF6E40),
-//           700: Color(0xFFFF6E40),
-//           800: Color(0xFFFF6E40),
-//           900: Color(0xFFFF6E40),
-//         }),
-//       ),
-//       home: const Login(),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'firebase_options.dart';
+import 'login.dart';
 
-Future<void> main() async {
+MaterialColor mainColor = MaterialColor(0xFFD06674, {
+  50: Color(0xFFD06674),
+  100: Color(0xFFD06674),
+  200: Color(0xFFD06674),
+  300: Color(0xFFD06674),
+  400: Color(0xFFD06674),
+  500: Color(0xFFD06674), // 500 is the primary color value
+  600: Color(0xFFD06674),
+  700: Color(0xFFD06674),
+  800: Color(0xFFD06674),
+  900: Color(0xFFD06674),
+});
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseMessaging.instance.getToken().then((token) {
+    print("Firebase Messaging Token: $token");
+  });
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String? fcmToken;
-
-  @override
-  void initState() {
-    super.initState();
-    getFCMToken();
-  }
-
-  Future<void> getFCMToken() async {
-    String? token = await FirebaseMessaging.instance.getToken();
-    setState(() {
-      fcmToken = token;
-    });
-    print("FCM Token: $token");
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter FCM Token Example',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('FCM Token Example'),
-        ),
-        body: Center(
-          child: Text(
-            'FCM Token: $fcmToken',
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
+      title: 'Barbercome App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: mainColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: const Login(),
     );
   }
 }
--- lib/register.dart
+++ lib/register.dart
@@ -1,4 +1,6 @@
+// register.dart
 import 'package:flutter/material.dart';
+import 'package:fancy_button_flutter/fancy_button_flutter.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -8,8 +10,145 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 47, 153, 139),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 100),
+              const Text(
+                "Register",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 50),
+              Container(
+                width: 300,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF29998B), // Teal background color
+                ),
+                child: TextFormField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(
+                    hintText: "Full Name",
+                    hintStyle: TextStyle(color: Colors.white38),
+                    prefixIcon: Icon(Icons.person, color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white38),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: 300,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF29998B), // Teal background color
+                ),
+                child: TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    hintText: "Email",
+                    hintStyle: TextStyle(color: Colors.white38),
+                    prefixIcon: Icon(Icons.email, color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white38),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: 300,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF29998B), // Teal background color
+                ),
+                child: TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "Password",
+                    hintStyle: TextStyle(color: Colors.white38),
+                    prefixIcon: Icon(Icons.lock, color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white38),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 40),
+              FancyButton(
+                button_text: "Register",
+                button_height: 30,
+                button_width: 100,
+                button_radius: 10,
+                button_color: Color.fromARGB(255, 30, 111, 102),
+                button_outline_color: Color.fromARGB(255, 25, 95, 86),
+                button_text_color: Colors.white,
+                button_icon_color: Colors.white,
+                icon_size: 22,
+                button_text_size: 15,
+                onClick: () {
+                  // Here you can add your registration logic
+                  print("Register button clicked");
+                },
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account?",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pop(); // Go back to the login page
+                    },
+                    child: Text(
+                      "Login here",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w900),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
